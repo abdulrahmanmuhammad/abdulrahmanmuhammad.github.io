8951b4f23daaa851e76efeb35137d7703161f82b
Muhammad Abdul Rahman <67320004+abdulrahmanmuhammad@users.noreply.github.com>
2024-02-07 13:20:48 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggle = document.getElementById("toggle");
-    const modeText = document.getElementById("mode-text");
-
-    toggle.addEventListener("change", function () {
-        document.body.classList.toggle("dark-mode");
-        modeText.innerText = document.body.classList.contains("dark-mode") ? "Dark Mode" : "Light Mode";
-    });
-});
